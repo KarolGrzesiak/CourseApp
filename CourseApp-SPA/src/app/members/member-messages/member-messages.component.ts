e71c9e6fb802e859022446035eefe4f2dbd78d83
KarolGrzesiak <karol.grzesiakk@gmail.com>
2018-12-16 02:45:11 +0100

    Finished messages components on client side

--- CourseApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ CourseApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from 'src/app/_models/message';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -6,10 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @Input() recipientId: number;
+  messages: Message[];
+  newMessage: any = {};
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) { }
 
   ngOnInit() {
+    this.loadMessages();
   }
 
+  loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
+    this.userService
+      .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(messages[i].id, currentUserId);
+            }
+          }
+        })
+      )
+      .subscribe(
+        messages => {
+          this.messages = messages;
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
+  }
+
+  sendMessage() {
+    this.newMessage.recipientId = this.recipientId;
+    this.userService.sendMessage(this.authService.decodedToken.nameid, this.newMessage).subscribe(
+      (message: Message) => {
+        this.messages.unshift(message);
+        this.newMessage.content = '';
+      },
+      error => this.alertify.error(error)
+    );
+  }
 }
